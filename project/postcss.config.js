63861795085b4d751f3bcb1407fd75a556118604
umesh batalvi <104628837+celestialcracker@users.noreply.github.com>
2025-05-20 23:20:53 +0100

    Update postcss.config.js

--- project/postcss.config.js
+++ project/postcss.config.js
@@ -1,3 +1,10 @@
+/**
+ * PostCSS Configuration File
+ * 
+ * This file configures PostCSS, a tool for transforming CSS with JavaScript plugins.
+ * PostCSS is used in this project to process CSS and apply various transformations.
+ */
+
 export default {
   plugins: {
     tailwindcss: {},
