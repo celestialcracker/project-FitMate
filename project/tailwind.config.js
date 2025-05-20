227b772ea8c9836bf013fb4df487e908283fd905
umesh batalvi <104628837+celestialcracker@users.noreply.github.com>
2025-05-20 23:21:16 +0100

    Update tailwind.config.js

--- project/tailwind.config.js
+++ project/tailwind.config.js
@@ -1,3 +1,11 @@
+/**
+ * Tailwind CSS Configuration File
+ * 
+ * This file configures Tailwind CSS, a utility-first CSS framework.
+ * It defines which files Tailwind should scan for classes and any customizations
+ * to the default theme.
+ */
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
